d1c9d8ec057922d42b9ee8b7d1064366449f0e9d
InferMS <edgarux5@gmail.com>
2023-12-02 18:06:05 +0100

    Better Script

--- src/java/utilities/db.sql
+++ src/java/utilities/db.sql
@@ -3,16 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
  */
 /**
- * Author:  edgar
+ * Author:  edgar y jordi
  * Created: 30 nov 2023
  */
+INSERT INTO CREDENTIALS (ID, USERNAME, PASSWORD) VALUES (1, 'sob', 'sob')
 
-INSERT INTO CONSOLE (ID, NAME) VALUES (1, 'GayStation')
-INSERT INTO CONSOLE (ID, NAME) VALUES (2, 'Nampero Swatch')
-INSERT INTO CONSOLE (ID, NAME) VALUES (3, 'NiggerBox')
-INSERT INTO CONSOLE (ID, NAME) VALUES (4, 'Master Race')
+/* Ejemplos para la clase GAME*/
 
-INSERT INTO GAMETYPE (ID, NAME) VALUES (1, 'Gay BDSM')
-INSERT INTO GAMETYPE (ID, NAME) VALUES (2, 'Furry')
-INSERT INTO GAMETYPE (ID, NAME) VALUES (3, 'Mario Cummer')
-INSERT INTO GAMETYPE (ID, NAME) VALUES (4, 'Sucking Cock')
+INSERT INTO GAME (ID, NAME, STOCK, DESCRIPTION, CITY, STATE, STREET, ZIP_CODE) VALUES (2, 'Super Mario Odyssey', 20, 'An amazing adventure game', 'Cityville', 'Stateville', '123 Main St', '12345')
+INSERT INTO GAME (ID, NAME, STOCK, DESCRIPTION, CITY, STATE, STREET, ZIP_CODE) VALUES (3, 'Call of Duty: Modern Warfare', 15, 'Intense first-person shooter', 'Cityville', 'Stateville', '123 Main St', '12345')
+INSERT INTO GAME (ID, NAME, STOCK, DESCRIPTION, CITY, STATE, STREET, ZIP_CODE) VALUES (4, 'The Witcher 3: Wild Hunt', 10, 'Epic open-world RPG', 'Townsville', 'Provinceville', '456 Oak St', '67890')
+INSERT INTO GAME (ID, NAME, STOCK, DESCRIPTION, CITY, STATE, STREET, ZIP_CODE) VALUES (5, 'FIFA 22', 30, 'Football simulation game', 'Townsville', 'Provinceville', '456 Oak St', '67890')
+
+/* Ejemplos para la clase CONSOLE*/
+
+INSERT INTO CONSOLE (ID, NAME) VALUES (1, 'PlayStation 5')
+INSERT INTO CONSOLE (ID, NAME) VALUES (2, 'Xbox Series X')
+INSERT INTO CONSOLE (ID, NAME) VALUES (3, 'Nintendo Switch')
+INSERT INTO CONSOLE (ID, NAME) VALUES (4, 'PC Gaming Master Race')
+
+/* Ejemplos para la clase CUSTOMER*/
+
+INSERT INTO CUSTOMER (ID, NAME, EMAIL, PASSWORD) VALUES ('AO123456', 'John Doe', 'john@example.com', 'john_password')
+INSERT INTO CUSTOMER (ID, NAME, EMAIL, PASSWORD) VALUES ('123456', 'Jane Doe', 'jane@example.com', 'jane_password')
+INSERT INTO CUSTOMER (ID, NAME, EMAIL, PASSWORD) VALUES ('ASDQWE', 'Alice Johnson', 'alice@example.com', 'alice_password')
+INSERT INTO CUSTOMER (ID, NAME, EMAIL, PASSWORD) VALUES ('QWERT631', 'Bob Smith', 'bob@example.com', 'bob_password')
+
+/* Ejemplos para la clase GAMETYPE*/
+
+INSERT INTO GAMETYPE (ID, NAME) VALUES (1, 'Action/Adventure')
+INSERT INTO GAMETYPE (ID, NAME) VALUES (2, 'First Person Shooter')
+INSERT INTO GAMETYPE (ID, NAME) VALUES (3, 'Role-Playing Game')
+INSERT INTO GAMETYPE (ID, NAME) VALUES (4, 'Sports')
