9aa5371c1019f79ba4cef28c2b0c79ada5c258f5
InferMS <edgarux5@gmail.com>
2024-01-06 23:40:22 +0100

    brrr

---
+++ src/java/service/ConsoleFacadeREST.java
@@ -0,0 +1,123 @@
+package service;
+
+import java.util.List;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import authn.Secured;
+import jakarta.persistence.Query;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
+import model.entities.Console;
+
+@Stateless
+@Path("console")
+public class ConsoleFacadeREST extends AbstractFacade<Console> {
+
+    @PersistenceContext(unitName = "Homework1PU")
+    private EntityManager em;
+
+    public ConsoleFacadeREST() {
+        super(Console.class);
+    }
+
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Console console) {
+        super.create(console);
+    }
+
+    @POST
+    @Secured
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response createConsole(Console console, @Context UriInfo uriInfo) {
+        if (consoleExists(console.getId())) {
+            return Response.status(Response.Status.CONFLICT).entity("Console already exists").build();
+        }
+  
+        super.create(console);
+
+        UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+        uriBuilder.path(Long.toString(console.getId()));
+
+        return Response.created(uriBuilder.build()).entity(super.find(console.getId())).build();
+    }
+
+    private boolean consoleExists(Long consoleId) {
+        Query findById = em.createNamedQuery("Console.findById", Console.class)
+                .setParameter("id", consoleId);
+
+        Console console = (Console) findById.getSingleResult();
+
+        return (console != null); 
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Long id, Console entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @Secured
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response find(@PathParam("id") Long id) {
+        return Response.ok().entity(super.find(id)).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response findById(@PathParam("id") Long id) {
+            Console console = super.find(id);
+            return Response.ok().entity(console).build();
+    }
+    
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getAll() {
+        Query findAll = em.createNamedQuery("Console.findAll", Console.class);
+
+        List<Console> consoles = findAll.getResultList();
+
+        return Response.ok().entity(consoles).build();  
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Console> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+    
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+}
